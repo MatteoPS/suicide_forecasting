42568ecd988a5ed0f3893eafbc961ede9ac05f54
Matteo Perini <perini.mtt@gmail.com>
2026-06-11 18:34:11 -0400

    re-arranginf config files

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,36 +1,26 @@
 import yaml
-import pandas as pd
 from pathlib import Path
+from functools import lru_cache
 
-# This dynamically finds the root of your repository, 
-# no matter where the code is run from.
 PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent
 
+@lru_cache(maxsize=None) #forces Python to memorize the dictionary the first time it loads. Subsequent calls take 0 milliseconds because it never reads the disk again.
 def load_config(config_name: str = "data.yaml") -> dict:
-    """Loads a YAML configuration file."""
+    """Loads and caches a YAML configuration file."""
     config_path = PROJECT_ROOT / "config" / config_name
     
+    if not config_path.exists():
+        raise FileNotFoundError(f"Config file missing: {config_path}")
+    
     with open(config_path, "r") as file:
-        config = yaml.safe_load(file)
-        
-    return config
+        return yaml.safe_load(file)
 
-def get_data_path(file_key: str, data_folder: str ) -> Path:
+def get_data_path(file_key: str, data_folder: str = "raw") -> Path:
     """Convenience function to get the absolute path to a data file."""
-    config = load_config()
-    relative_path = config["paths"][data_folder][file_key]
-    return PROJECT_ROOT / relative_path
-
-def load_nvdrs(file_key: str, data_folder: str, nrows: int = None) -> pd.DataFrame:
-    nvdrs_path = get_data_path(file_key, data_folder)
+    config = load_config("data.yaml")
     
-    # Add the encoding parameter here
-    df = pd.read_csv(
-        nvdrs_path, 
-        encoding="cp1252", 
-        encoding_errors="replace", #if files have some encoding issues, this will replace problematic characters instead of throwing an error
-        low_memory=False,  # for large files
-        nrows=nrows  # to limit the number of rows read
-    )
-    
-    return df
+    try:
+        relative_path = config["paths"][data_folder][file_key]
+        return PROJECT_ROOT / relative_path
+    except KeyError:
+        raise KeyError(f"Could not find paths -> {data_folder} -> {file_key} in data.yaml")
