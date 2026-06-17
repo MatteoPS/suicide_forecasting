5aa9208537c9d6716e6ad2a69797f286aac78f30
Matteo Perini <perini.mtt@gmail.com>
2026-06-17 17:50:34 -0400

    load HCUP

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,10 +1,24 @@
+"""Global configuration, paths, and environment variables."""
 import yaml
+import os
 from pathlib import Path
 from functools import lru_cache
+from dotenv import load_dotenv
 
+# Load environment variables from .env
+load_dotenv()
+
+# --- Paths & Configuration ---
 PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent
 
-@lru_cache(maxsize=None) #forces Python to memorize the dictionary the first time it loads. Subsequent calls take 0 milliseconds because it never reads the disk again.
+# --- Redivis Credentials ---
+USERNAME = os.getenv("REDIVIS_USERNAME")
+ORGNAME = os.getenv("REDIVIS_ORGANIZATION")
+if not USERNAME or not ORGNAME:
+    raise ValueError("Missing REDIVIS_USERNAME or REDIVIS_ORGANIZATION in .env file.")
+
+@lru_cache(maxsize=None) # forces Python to memorize the dictionary the first time it loads.
+
 def load_config(config_name: str = "data.yaml") -> dict:
     """Loads and caches a YAML configuration file."""
     config_path = PROJECT_ROOT / "config" / config_name
@@ -24,3 +38,4 @@ def get_data_path(file_key: str, data_folder: str = "raw") -> Path:
         return PROJECT_ROOT / relative_path
     except KeyError:
         raise KeyError(f"Could not find paths -> {data_folder} -> {file_key} in data.yaml")
+        
